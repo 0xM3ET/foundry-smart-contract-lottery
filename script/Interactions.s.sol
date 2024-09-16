41f50a1f0cc81e57e0761b15b321deddc49e09e3
0xMrPanda <meetpandya.dev@gmail.com>
2024-09-16 23:31:04 +0530

    added Subscriptions

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,10 +1,35 @@
 // SPDX-License-Identifier:MIT
 pragma solidity 0.8.19;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
-contract CrateSubscription is Script {
-    function createSubscriptionUsingConfig() public {}
+contract CreateSubscription is Script {
+    function createSubscriptionUsingConfig() public returns (uint256, address) {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig()._vrfCoordiantor;
+
+        // Create and Return a subscription
+        (uint256 subId, ) = createSubscription(vrfCoordinator);
+        return (subId, vrfCoordinator);
+    }
+
+    function createSubscription(
+        address vrfCoordinator
+    ) public returns (uint256, address) {
+        console.log("Create Subscription on Chain Id: ", block.chainid);
+        vm.startBroadcast();
+        uint256 subId = VRFCoordinatorV2_5Mock(vrfCoordinator)
+            .createSubscription();
+        vm.stopBroadcast();
+        console.log("Your subscription Id is: ", subId);
+        console.log(
+            "Please update the subscription Id in yout Helperconfig.s.sol"
+        );
+
+        return (subId, vrfCoordinator);
+    }
 
     function run() public {
         createSubscriptionUsingConfig();
